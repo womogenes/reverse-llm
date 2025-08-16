ae4ebe66d0a5e51b54610db8ff8c3f3d421664c6
William Y. Feng <womogenes2@gmail.com>
2025-08-15 23:07:22 -0400

    Regroup and start doing modal (yay)

--- load_data/load_data.py
+++ load_data/load_data.py
@@ -1,57 +1,39 @@
-from datasets import load_dataset, Dataset, DatasetDict
-
+from datasets import load_dataset, DatasetDict
+import time
 import os
+
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+os.environ["HF_DATASETS_CACHE"] = "/mnt/william/.cache"
 
-n_samples = 2_000_000
-context_length = 1024
+### CONFIG ###
 
+dataset_name = "fineweb-10BT"
 
+DATA_DIR = "/mnt/william/reverse-llm/data"
+TOKENIZER_DIR = "/mnt/william/reverse-llm/tokenizers"
 
-DATA_DIR = "/home/wyf/ai/causal-llm/data"
+### CONFIG ###
 
-# dataset = "dclm"
-dataset_name = "fineweb"
+if __name__ == "__main__":
+    print(f"Downloading dataset {dataset_name}...")
+    time.sleep(1)
 
-# Takes like 30s to load (it's bad)
-raw_dataset = load_dataset(
-    # "mlfoundations/dclm-baseline-1.0",
-    "HuggingFaceFW/fineweb-edu",
-    split="train",
-    streaming=True,
-)
-
-
-from tqdm import tqdm
-
-def filter_dataset(dataset):
-    # filtered = []
-    # for sample in tqdm(iter(dataset["train"].take(n_samples)), total=n_samples):
-    #     # IMPORTANT REVERSAL STEP
-    #     filtered.append(sample["text"][::-1])
-    # return filtered
-
-    return (
-        dataset
-            .select_columns(["text"])
-            .map(lambda s: {"text": s["text"][::-1]})
+    # Takes like 30s to load (it's bad)
+    raw_dataset = load_dataset(
+        "HuggingFaceFW/fineweb-edu",
+        split="train",
+        name="sample-10BT",
+        cache_dir="/mnt/william/.cache",
     )
-    
-# 1k examples: 4.0s
 
-print("Generating split datasets...")
-raw_dataset_with_tqdm = [x for x in tqdm(raw_dataset.take(n_samples), total=n_samples)]
-split_datasets = (
-    Dataset.from_list(list(raw_dataset_with_tqdm))
-        .train_test_split(test_size=0.1, seed=0)
-)
-datasets = DatasetDict({
-    "train": filter_dataset(split_datasets["train"]),
-    "valid": filter_dataset(split_datasets["test"]),
-})
+    print("Generating split datasets...")
+    split_datasets = raw_dataset.train_test_split(test_size=0.005, seed=0)
+    split_datasets = DatasetDict({
+        "train": split_datasets["train"],
+        "valid": split_datasets["test"],
+    })
 
-
-print("Saving datasets...")
-for split_name, dataset in datasets.items():
-    dataset.to_parquet(f"{DATA_DIR}/{dataset_name}_{n_samples}/{split_name}.parquet")
+    print("=== SAMPLE DATA ===")
+    print(list(split_datasets["train"].take(1))[0]["text"][500::-1])
+    print()
--- load_data/load_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH -p mit_normal
-#SBATCH --mincpus 64
-#SBATCH --mem 256000
-
-module load miniforge
-
-/home/wyf/.conda/envs/torch/bin/python load_data.py
